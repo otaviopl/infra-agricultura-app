b3f877b11b96631fee0b77ba8f1ebd64554bed2e
otaviopl <otavio.pereira.lopes@gmail.com>
2024-11-17 17:46:31 -0300

    Initial commit

---
+++ test/infra-agricultura.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfraAgricultura from '../lib/infra-agricultura-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infra-agricultura-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfraAgricultura.InfraAgriculturaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
